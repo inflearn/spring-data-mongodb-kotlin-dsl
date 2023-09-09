f9b3e6bde2735b1b5e6f081874b9a947c5f4c083
Jake Son <sonjeabin@gmail.com>
2023-09-09 23:29:45 +0900

    feat: add count stage to AggregationDsl

--- core/src/main/kotlin/com/github/inflab/spring/data/mongodb/core/aggregation/AggregationDsl.kt
+++ core/src/main/kotlin/com/github/inflab/spring/data/mongodb/core/aggregation/AggregationDsl.kt
@@ -2,6 +2,7 @@ package com.github.inflab.spring.data.mongodb.core.aggregation
 
 import com.github.inflab.spring.data.mongodb.core.annotation.AggregationMarker
 import org.springframework.data.mongodb.core.aggregation.Aggregation
+import org.springframework.data.mongodb.core.aggregation.AggregationOperation
 
 /**
  * A Kotlin DSL to configure [Aggregation] using idiomatic Kotlin code.
@@ -11,8 +12,18 @@ import org.springframework.data.mongodb.core.aggregation.Aggregation
  */
 @AggregationMarker
 class AggregationDsl {
+    private val operations = mutableListOf<AggregationOperation>()
 
-    fun build(): Aggregation {
-        return Aggregation.newAggregation()
+    /**
+     * Passes a document to the next stage that contains a count of the number of documents input to the stage.
+     *
+     * @param fieldName The name of the output field which has the count as its value.
+     * @see <a href="https://docs.mongodb.com/manual/reference/operator/aggregation/count/#pipe._S_count">https://docs.mongodb.com/manual/reference/operator/aggregation/count/</a>
+     */
+    fun count(fieldName: String) {
+        operations += Aggregation.count().`as`(fieldName)
     }
+
+    fun build(): Aggregation =
+        Aggregation.newAggregation(operations)
 }
