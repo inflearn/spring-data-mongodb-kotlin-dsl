6cc907c2284aeaddf6e9292c7e19c42ab016c890
Jake Son <sonjeabin@gmail.com>
2023-09-10 19:20:29 +0900

    feat: add search stage to AggregationDsl

--- core/src/main/kotlin/com/github/inflab/spring/data/mongodb/core/aggregation/AggregationDsl.kt
+++ core/src/main/kotlin/com/github/inflab/spring/data/mongodb/core/aggregation/AggregationDsl.kt
@@ -1,5 +1,6 @@
 package com.github.inflab.spring.data.mongodb.core.aggregation
 
+import com.github.inflab.spring.data.mongodb.core.aggregation.search.SearchStageDsl
 import com.github.inflab.spring.data.mongodb.core.annotation.AggregationMarker
 import org.springframework.data.mongodb.core.aggregation.Aggregation
 import org.springframework.data.mongodb.core.aggregation.AggregationOperation
@@ -18,10 +19,21 @@ class AggregationDsl {
      * Passes a document to the next stage that contains a count of the number of documents input to the stage.
      *
      * @param fieldName The name of the output field which has the count as its value.
-     * @see <a href="https://docs.mongodb.com/manual/reference/operator/aggregation/count/#pipe._S_count">https://docs.mongodb.com/manual/reference/operator/aggregation/count/</a>
+     * @see <a href="https://docs.mongodb.com/manual/reference/operator/aggregation/count/#pipe._S_count">$count (aggregation)</a>
      */
     fun count(fieldName: String) {
         operations += Aggregation.count().`as`(fieldName)
+    }
+
+    /**
+     * Configures a stage that performs a full-text search on the specified field or fields which must be covered by an Atlas Search index.
+     *
+     * @param searchConfiguration custom configurations for the search stage
+     * @see <a href="https://docs.mongodb.com/manual/reference/operator/aggregation/search">$search (aggregation)</a>
+     * @see <a href="https://www.mongodb.com/docs/atlas/atlas-search/query-syntax/">Return Atlas Search Results or Metadata</a>
+     */
+    fun search(searchConfiguration: SearchStageDsl.() -> Unit) {
+        operations += SearchStageDsl().apply(searchConfiguration).build()
     }
 
     fun build(): Aggregation =
--- core/src/test/kotlin/com/github/inflab/spring/data/mongodb/core/aggregation/AggregationDslTest.kt
+++ core/src/test/kotlin/com/github/inflab/spring/data/mongodb/core/aggregation/AggregationDslTest.kt
@@ -1,7 +1,9 @@
 package com.github.inflab.spring.data.mongodb.core.aggregation
 
+import com.github.inflab.spring.data.mongodb.core.aggregation.search.SearchOperation
 import io.kotest.core.spec.style.FreeSpec
 import io.kotest.matchers.shouldBe
+import org.bson.Document
 import org.springframework.data.mongodb.core.aggregation.Aggregation
 
 internal class AggregationDslTest : FreeSpec({
@@ -14,6 +16,20 @@ internal class AggregationDslTest : FreeSpec({
 
             aggregation.toString() shouldBe Aggregation.newAggregation(
                 Aggregation.count().`as`("fieldName"),
+            ).toString()
+        }
+    }
+
+    "search" - {
+        "should create search stage with index option" {
+            val aggregation = aggregation {
+                search {
+                    index = "indexName"
+                }
+            }
+
+            aggregation.toString() shouldBe Aggregation.newAggregation(
+                SearchOperation(Document("index", "indexName")),
             ).toString()
         }
     }
