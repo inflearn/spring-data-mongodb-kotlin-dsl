d7db3d23b97fadea2e1b1b3aec33859971ac688d
username1103 <mikim961103@gmail.com>
2023-09-18 10:09:34 +0900

    feat: implement equals search operator

--- core/src/main/kotlin/com/github/inflab/spring/data/mongodb/core/aggregation/search/SearchOperator.kt
+++ core/src/main/kotlin/com/github/inflab/spring/data/mongodb/core/aggregation/search/SearchOperator.kt
@@ -20,4 +20,11 @@ interface SearchOperator {
      * @see <a href="https://www.mongodb.com/docs/atlas/atlas-search/text/#text">text</a>
      */
     fun text(configuration: TextSearchOperatorDsl.() -> Unit)
+
+    /**
+     * checks whether a field matches a value you specify. equals supports querying the following data types:
+     * boolean, objectId,  number,  including int32, int64, and double, date
+     * You can use the equals operator to query booleans, objectIds, numbers, and dates in arrays. If at least one element in the array matches the "value" field in the equals operator, Atlas Search adds the document to the result set.
+     */
+    fun equal(configuration: EqualsSearchOperatorDsl.() -> Unit)
 }
