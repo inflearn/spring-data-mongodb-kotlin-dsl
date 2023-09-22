39656e938e8e83797b67535f2764754b900ce3be
Jake Son <sonjeabin@gmail.com>
2023-09-23 08:39:13 +0900

    chore: add example module

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,5 +3,8 @@ plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.4.0"
 }
 
+enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
+
 rootProject.name = "spring-data-mongodb-kotlin-dsl"
 include("core")
+include("example")
