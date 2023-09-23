a9c5c23b20de71d7fb1cee84849ce75f086d11ea
Jake Son <sonjeabin@gmail.com>
2023-09-23 11:27:05 +0900

    feat: init spring boot

--- example/build.gradle.kts
+++ example/build.gradle.kts
@@ -1,10 +1,20 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
+plugins {
+    alias(exampleLibs.plugins.spring.boot)
+    alias(exampleLibs.plugins.spring.dependency.management)
+    alias(exampleLibs.plugins.kotlin.spring)
+}
+
 dependencies {
     implementation(projects.core)
+    implementation(exampleLibs.kotlin.reflect)
     implementation(exampleLibs.spring.data.mongodb)
+
+    testImplementation(exampleLibs.spring.test)
 }
 
 java {
-    withSourcesJar()
     toolchain {
         languageVersion.set(JavaLanguageVersion.of(17))
     }
@@ -14,4 +24,12 @@ kotlin {
     jvmToolchain {
         languageVersion.set(JavaLanguageVersion.of(17))
     }
+    compilerOptions {
+        freeCompilerArgs.set(listOf("-Xjsr305=strict"))
+        jvmTarget.set(JvmTarget.JVM_17)
+    }
+}
+
+tasks.jar {
+    enabled = false
 }
