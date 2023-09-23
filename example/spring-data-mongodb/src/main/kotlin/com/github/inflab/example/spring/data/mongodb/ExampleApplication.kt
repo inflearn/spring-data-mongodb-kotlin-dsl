330c0bff7fb3fc38e002a733f9d17fe17394605a
Jake Son <sonjeabin@gmail.com>
2023-09-23 12:59:10 +0900

    feat: add example text search

--- example/spring-data-mongodb/src/main/kotlin/com/github/inflab/example/spring/data/mongodb/ExampleApplication.kt
+++ example/spring-data-mongodb/src/main/kotlin/com/github/inflab/example/spring/data/mongodb/ExampleApplication.kt
@@ -1,8 +1,10 @@
 package com.github.inflab.example.spring.data.mongodb
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
 
+@ConfigurationPropertiesScan
 @SpringBootApplication
 class ExampleApplication
 
---
+++ example/spring-data-mongodb/src/main/kotlin/com/github/inflab/example/spring/data/mongodb/entity/mflix/Movies.kt
@@ -0,0 +1,27 @@
+package com.github.inflab.example.spring.data.mongodb.entity.mflix
+
+import org.springframework.data.annotation.Id
+import org.springframework.data.mongodb.core.mapping.Document
+import java.time.LocalDateTime
+
+@Document("movies")
+data class Movies(
+    @Id
+    val id: String,
+    val plot: String,
+    val genres: List<String>,
+    val runtime: Int,
+    val cast: List<String>,
+    val poster: String,
+    val title: String,
+    val fullplot: String,
+    val languages: List<String>,
+    val released: LocalDateTime,
+    val directors: List<String>,
+    val rated: String,
+    val lastupdated: LocalDateTime,
+    val year: Int,
+    val contries: List<String>,
+    val type: String,
+    val numMflixComments: Int,
+)
