f1146015a79a24c06ae73eae4f6de03c688e5eec
Jake Son <sonjeabin@gmail.com>
2023-09-24 12:18:56 +0900

    test: add range search example

---
+++ example/spring-data-mongodb/src/main/kotlin/com/github/inflab/example/spring/data/mongodb/search/range/RangeSearchRepository.kt
@@ -0,0 +1,76 @@
+package com.github.inflab.example.spring.data.mongodb.search.range
+
+import com.github.inflab.example.spring.data.mongodb.entity.mflix.Movies
+import com.github.inflab.spring.data.mongodb.core.aggregation.aggregation
+import org.springframework.data.mongodb.core.MongoTemplate
+import org.springframework.data.mongodb.core.aggregate
+import org.springframework.data.mongodb.core.aggregation.AggregationResults
+import org.springframework.stereotype.Repository
+import java.time.LocalDate
+import java.time.LocalDateTime
+
+@Repository
+class RangeSearchRepository(
+    private val mongoTemplate: MongoTemplate,
+) {
+
+    data class TitleAndRuntimeDto(
+        val title: String,
+        val runtime: Int,
+    )
+
+    /**
+     * @see <a href="https://www.mongodb.com/docs/atlas/atlas-search/range/#number-example">Number Example</a>
+     */
+    fun findRuntimeBetween(): AggregationResults<TitleAndRuntimeDto> {
+        val aggregation = aggregation {
+            search {
+                range {
+                    path(Movies::runtime)
+                    gte(2)
+                    lte(3)
+                }
+            }
+
+            // TODO: add $limit stage
+
+            project {
+                excludeId()
+                +Movies::title
+                +Movies::runtime
+            }
+        }
+
+        return mongoTemplate.aggregate<Movies, TitleAndRuntimeDto>(aggregation)
+    }
+
+    data class TitleAndReleasedDto(
+        val title: String,
+        val released: LocalDateTime,
+    )
+
+    /**
+     * @see <a href="https://www.mongodb.com/docs/atlas/atlas-search/range/#date-example">Date Example</a>
+     */
+    fun findReleasedBetween(): AggregationResults<TitleAndReleasedDto> {
+        val aggregation = aggregation {
+            search {
+                range {
+                    path(Movies::released)
+                    gt(LocalDate.of(2010, 1, 1))
+                    lt(LocalDate.of(2015, 1, 1))
+                }
+            }
+
+            // TODO: add $limit stage
+
+            project {
+                excludeId()
+                +Movies::title
+                +Movies::released
+            }
+        }
+
+        return mongoTemplate.aggregate<Movies, TitleAndReleasedDto>(aggregation)
+    }
+}
