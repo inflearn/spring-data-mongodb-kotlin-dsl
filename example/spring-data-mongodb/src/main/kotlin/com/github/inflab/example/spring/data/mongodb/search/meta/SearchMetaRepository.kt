f73989e41953edb9e4f5f5a47e628e48bc284bf7
Jake Son <sonjeabin@gmail.com>
2023-09-24 19:34:32 +0900

    test: add search meta example

---
+++ example/spring-data-mongodb/src/main/kotlin/com/github/inflab/example/spring/data/mongodb/search/meta/SearchMetaRepository.kt
@@ -0,0 +1,33 @@
+package com.github.inflab.example.spring.data.mongodb.search.meta
+
+import com.github.inflab.example.spring.data.mongodb.entity.mflix.Movies
+import com.github.inflab.spring.data.mongodb.core.aggregation.aggregation
+import org.springframework.data.mongodb.core.MongoTemplate
+import org.springframework.data.mongodb.core.aggregate
+import org.springframework.data.mongodb.core.aggregation.AggregationResults
+
+class SearchMetaRepository(
+    private val mongoTemplate: MongoTemplate,
+) {
+
+    data class Count(val total: Long)
+    data class CountDto(val count: Count)
+
+    /**
+     * @see <a href="https://www.mongodb.com/docs/atlas/atlas-search/query-syntax/#example">Example</a>
+     */
+    fun findCountBetweenYear(): AggregationResults<CountDto> {
+        val aggregation = aggregation {
+            searchMeta {
+                range {
+                    path("year")
+                    gte(1998)
+                    lt(1999)
+                }
+                totalCount()
+            }
+        }
+
+        return mongoTemplate.aggregate<Movies, CountDto>(aggregation)
+    }
+}
---
+++ example/spring-data-mongodb/src/test/kotlin/com/github/inflab/example/spring/data/mongodb/search/meta/SearchMetaRepositoryTest.kt
@@ -0,0 +1,22 @@
+package com.github.inflab.example.spring.data.mongodb.search.meta
+
+import io.kotest.core.annotation.Ignored
+import io.kotest.core.spec.style.FreeSpec
+import io.kotest.matchers.longs.shouldBeGreaterThan
+import org.springframework.data.mongodb.core.MongoTemplate
+import org.springframework.data.mongodb.core.SimpleMongoClientDatabaseFactory
+
+@Ignored
+internal class SearchMetaRepositoryTest : FreeSpec({
+    val connectionString = "mongodb+srv://<username>:<password>@<host>/sample_mflix?retryWrites=true&w=majority"
+    val mongoTemplate = MongoTemplate(SimpleMongoClientDatabaseFactory(connectionString))
+    val searchMetaRepository = SearchMetaRepository(mongoTemplate)
+
+    "findCountBetweenYear" {
+        // when
+        val result = searchMetaRepository.findCountBetweenYear()
+
+        // then
+        result.mappedResults.first().count.total.shouldBeGreaterThan(500)
+    }
+})
