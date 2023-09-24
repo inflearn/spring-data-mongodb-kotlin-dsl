1a1e0aac9e9e487fa307024f6620ac7d139ce9cb
Jake Son <sonjeabin@gmail.com>
2023-09-24 09:11:09 +0900

    test: add project stage to example query

--- example/spring-data-mongodb/src/test/kotlin/com/github/inflab/example/spring/data/mongodb/search/text/TextSearchRepositoryTest.kt
+++ example/spring-data-mongodb/src/test/kotlin/com/github/inflab/example/spring/data/mongodb/search/text/TextSearchRepositoryTest.kt
@@ -2,14 +2,14 @@ package com.github.inflab.example.spring.data.mongodb.search.text
 
 import io.kotest.core.annotation.Ignored
 import io.kotest.core.spec.style.FreeSpec
-import io.kotest.matchers.collections.shouldHaveSize
+import io.kotest.matchers.collections.shouldBeStrictlyDecreasing
 import io.kotest.matchers.equals.shouldBeEqual
 import org.springframework.data.mongodb.core.MongoTemplate
 import org.springframework.data.mongodb.core.SimpleMongoClientDatabaseFactory
 
 @Ignored
 internal class TextSearchRepositoryTest : FreeSpec({
-    val connectionString = ""
+    val connectionString = "mongodb+srv://<username>:<password>@<host>/sample_mflix?retryWrites=true&w=majority"
     val mongoTemplate = MongoTemplate(SimpleMongoClientDatabaseFactory(connectionString))
     val textSearchRepository = TextSearchRepository(mongoTemplate)
 
@@ -18,11 +18,11 @@ internal class TextSearchRepositoryTest : FreeSpec({
         val result = textSearchRepository.findTitleSufer()
 
         // then
-        result.mappedResults shouldHaveSize 3
         result.mappedResults.map { it.title } shouldBeEqual listOf(
             "Soul Surfer",
             "Little Surfer Girl",
             "Fantastic 4: Rise of the Silver Surfer",
         )
+        result.mappedResults.map { it.score }.shouldBeStrictlyDecreasing()
     }
 })
