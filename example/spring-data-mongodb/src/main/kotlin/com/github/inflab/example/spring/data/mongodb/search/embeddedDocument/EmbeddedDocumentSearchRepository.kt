271c7d5d519645d5301f5031335adae28b4a68a2
Jake Son <sonjeabin@gmail.com>
2023-09-24 17:47:08 +0900

    test: add embeddedDocument examples

---
+++ example/spring-data-mongodb/src/main/kotlin/com/github/inflab/example/spring/data/mongodb/search/embeddedDocument/EmbeddedDocumentSearchRepository.kt
@@ -0,0 +1,113 @@
+package com.github.inflab.example.spring.data.mongodb.search.embeddedDocument
+
+import com.github.inflab.example.spring.data.mongodb.entity.supplies.SaleItem
+import com.github.inflab.example.spring.data.mongodb.entity.supplies.Sales
+import com.github.inflab.spring.data.mongodb.core.aggregation.aggregation
+import com.github.inflab.spring.data.mongodb.core.aggregation.search.ScoreEmbeddedAggregateStrategy
+import org.springframework.data.mongodb.core.MongoTemplate
+import org.springframework.data.mongodb.core.aggregate
+import org.springframework.data.mongodb.core.aggregation.AggregationResults
+import org.springframework.stereotype.Repository
+
+@Repository
+class EmbeddedDocumentSearchRepository(
+    private val mongoTemplate: MongoTemplate,
+) {
+
+    data class Item(
+        val name: String,
+        val tags: List<String>,
+    )
+
+    data class ItemDto(
+        val items: List<Item>,
+        val score: Double,
+    )
+
+    /**
+     * @see <a href="https://www.mongodb.com/docs/atlas/atlas-search/embedded-document/#examples">Basic Example</a>
+     */
+    fun findItemsByMean(): AggregationResults<ItemDto> {
+        val aggregation = aggregation {
+            search {
+                embeddedDocument {
+                    path(Sales::items)
+                    operator {
+                        compound {
+                            must {
+                                text {
+                                    path { +(Sales::items..SaleItem::tags) }
+                                    query("school")
+                                }
+                            }
+                            should {
+                                text {
+                                    path { +(Sales::items..SaleItem::name) }
+                                    query("backpack")
+                                }
+                            }
+                        }
+                    }
+                    score {
+                        embedded(ScoreEmbeddedAggregateStrategy.MEAN)
+                    }
+                }
+            }
+
+            // TODO: add $limit stage
+
+            project {
+                excludeId()
+                Sales::items..SaleItem::name alias "items.name"
+                Sales::items..SaleItem::tags alias "items.tags"
+                searchScore()
+            }
+        }
+
+        return mongoTemplate.aggregate<Sales, ItemDto>(aggregation)
+    }
+
+    data class FacetDto(val count: Count, val facet: Facet)
+    data class Count(val lowerBound: Long)
+    data class Facet(val purchaseMethodFacet: PurchaseMethodFacet)
+    data class PurchaseMethodFacet(val buckets: List<Bucket>)
+    data class Bucket(val id: String, val count: Long)
+
+    /**
+     * @see <a href="https://www.mongodb.com/docs/atlas/atlas-search/embedded-document/#examples">Facet Example</a>
+     */
+    fun findItemsByFacet(): AggregationResults<FacetDto> {
+        val aggregation = aggregation {
+            searchMeta {
+                facet {
+                    operator {
+                        embeddedDocument {
+                            path(Sales::items)
+                            operator {
+                                compound {
+                                    must {
+                                        text {
+                                            path { +(Sales::items..SaleItem::tags) }
+                                            query("school")
+                                        }
+                                    }
+                                    should {
+                                        text {
+                                            path { +(Sales::items..SaleItem::name) }
+                                            query("backpack")
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    "purchaseMethodFacet" stringFacet {
+                        path(Sales::purchaseMethod)
+                    }
+                }
+            }
+        }
+
+        return mongoTemplate.aggregate<Sales, FacetDto>(aggregation)
+    }
+}
---
+++ example/spring-data-mongodb/src/test/kotlin/com/github/inflab/example/spring/data/mongodb/search/embeddedDocument/EmbeddedDocumentSearchRepositoryTest.kt
@@ -0,0 +1,46 @@
+package com.github.inflab.example.spring.data.mongodb.search.embeddedDocument
+
+import io.kotest.core.annotation.Ignored
+import io.kotest.core.spec.style.FreeSpec
+import io.kotest.matchers.collections.shouldBeMonotonicallyDecreasing
+import io.kotest.matchers.longs.shouldBeGreaterThan
+import io.kotest.matchers.shouldBe
+import org.springframework.data.mongodb.core.MongoTemplate
+import org.springframework.data.mongodb.core.SimpleMongoClientDatabaseFactory
+
+@Ignored
+internal class EmbeddedDocumentSearchRepositoryTest : FreeSpec({
+    val connectionString = "mongodb+srv://<username>:<password>@<host>/sample_supplies?retryWrites=true&w=majority"
+    val mongoTemplate = MongoTemplate(SimpleMongoClientDatabaseFactory(connectionString))
+    val embeddedDocumentSearchRepository = EmbeddedDocumentSearchRepository(mongoTemplate)
+
+    "findItemsByMean" {
+        // when
+        val result = embeddedDocumentSearchRepository.findItemsByMean()
+
+        // then
+        result.mappedResults.take(5).flatMap { it.items } shouldBe listOf(
+            EmbeddedDocumentSearchRepository.Item("backpack", listOf("school", "travel", "kids")),
+            EmbeddedDocumentSearchRepository.Item("envelopes", listOf("stationary", "office", "general")),
+            EmbeddedDocumentSearchRepository.Item("printer paper", listOf("office", "stationary")),
+            EmbeddedDocumentSearchRepository.Item("backpack", listOf("school", "travel", "kids")),
+            EmbeddedDocumentSearchRepository.Item("backpack", listOf("school", "travel", "kids")),
+            EmbeddedDocumentSearchRepository.Item("backpack", listOf("school", "travel", "kids")),
+            EmbeddedDocumentSearchRepository.Item("backpack", listOf("school", "travel", "kids")),
+        )
+        result.mappedResults.take(5).map { it.score }.shouldBeMonotonicallyDecreasing()
+    }
+
+    "findItemsByFacet" {
+        // when
+        val result = embeddedDocumentSearchRepository.findItemsByFacet()
+
+        // then
+        result.mappedResults.first().count.lowerBound.shouldBeGreaterThan(0)
+        result.mappedResults.first().facet.purchaseMethodFacet.buckets shouldBe listOf(
+            EmbeddedDocumentSearchRepository.Bucket("In store", 2751),
+            EmbeddedDocumentSearchRepository.Bucket("Online", 1535),
+            EmbeddedDocumentSearchRepository.Bucket("Phone", 578),
+        )
+    }
+})
