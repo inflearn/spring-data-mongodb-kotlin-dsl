8d57bf293b31b77c780062c757a9b6f364bcef82
cranemont <cranemont@gmail.com>
2023-09-26 21:52:45 +0900

    style: run ktlint
    Co-authored-by: Jake Son <sonjeabin@gmail.com>

--- example/spring-data-mongodb/src/main/kotlin/com/github/inflab/example/spring/data/mongodb/entity/sample/FruitQuantities.kt
+++ example/spring-data-mongodb/src/main/kotlin/com/github/inflab/example/spring/data/mongodb/entity/sample/FruitQuantities.kt
@@ -3,5 +3,5 @@ package com.github.inflab.example.spring.data.mongodb.entity.sample
 data class FruitQuantities(
     val lemons: Int,
     val oranges: Int,
-    val grapefruit: Int
+    val grapefruit: Int,
 )
