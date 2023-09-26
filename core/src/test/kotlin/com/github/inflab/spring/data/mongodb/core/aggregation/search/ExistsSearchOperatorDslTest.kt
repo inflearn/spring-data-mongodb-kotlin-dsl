1ea883bf40691b0a9e1a2024fa61c603ffdd5dfd
cranemont <cranemont@gmail.com>
2023-09-26 21:51:10 +0900

    test: add test case for ExistsSearchOperatorDsl
    Co-authored-by: Jake Son <sonjeabin@gmail.com>

---
+++ core/src/test/kotlin/com/github/inflab/spring/data/mongodb/core/aggregation/search/ExistsSearchOperatorDslTest.kt
@@ -0,0 +1,87 @@
+package com.github.inflab.spring.data.mongodb.core.aggregation.search
+
+import com.github.inflab.spring.data.mongodb.core.util.shouldBeJson
+import io.kotest.core.spec.style.FreeSpec
+import io.kotest.matchers.shouldBe
+
+internal class ExistsSearchOperatorDslTest : FreeSpec({
+    fun exists(block: ExistsSearchOperatorDsl.() -> Unit): ExistsSearchOperatorDsl =
+        ExistsSearchOperatorDsl().apply(block)
+
+    "path" - {
+        "should set path by strings" {
+            // given
+            val operator = exists {
+                path("path")
+            }
+
+            // when
+            val result = operator.build()
+
+            // then
+            result.shouldBeJson(
+                """
+                {
+                  "exists": {
+                    "path": "path"
+                  }
+                }
+                """.trimIndent(),
+            )
+        }
+
+        "should set path by property" {
+            // given
+            data class Test (
+                val property: String
+            )
+
+            val operator = exists {
+                path(Test::property)
+            }
+
+            // when
+            val result = operator.build()
+
+            // then
+            result.shouldBeJson(
+                """
+                {
+                  "exists": {
+                    "path": "property"
+                  }
+                }
+                """.trimIndent(),
+            )
+        }
+    }
+
+    "score" - {
+        "should set score by score option" {
+            // given
+            val operator = exists {
+                score {
+                    boost(1.0)
+                }
+            }
+
+            // when
+            val result = operator.build()
+
+            // then
+            result.shouldBeJson(
+                """
+                {
+                  "exists": {
+                    "score": {
+                      "boost": {
+                        "value": 1.0
+                      }
+                    }
+                  }
+                }
+                """.trimIndent(),
+            )
+        }
+    }
+})
