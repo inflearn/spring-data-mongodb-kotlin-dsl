e54ca554ba1727f1f75399840e9dc529f0d9cfea
minwoo <mwoodev526@gmail.com>
2023-09-30 17:46:44 +0900

    doc: add documentation for queryString operator

--- core/src/main/kotlin/com/github/inflab/spring/data/mongodb/core/aggregation/search/QueryStringQueryOptionDsl.kt
+++ core/src/main/kotlin/com/github/inflab/spring/data/mongodb/core/aggregation/search/QueryStringQueryOptionDsl.kt
@@ -22,23 +22,57 @@ class QueryStringQueryOptionDsl {
         }
     }
 
+    /**
+     * Indicates 0 or more characters to match.
+     */
     val WILDCARD = "*"
+
+    /**
+     * Indicates any single character to match.
+     */
     val QUESTION = "?"
 
+    /**
+     * Creates a text query.
+     *
+     * @param value The value to search
+     * @param field Indexed field search
+     */
     fun text(value: String, field: String? = null): Query {
         val escaped = value.replace("*", "\\*").replace("?", "\\?")
 
         return Query("\"$escaped\"", field)
     }
 
+    /**
+     * Creates a wildcard query.
+     *
+     * @param value The value to search
+     * @param field Indexed field to search
+     */
     fun wildcard(value: String, field: String? = null): Query {
         return Query("\"$value\"", field)
     }
 
+    /**
+     * Creates a regex query.
+     *
+     * @param pattern The pattern to search
+     * @param field Indexed field to search
+     */
     fun regex(pattern: String, field: String? = null): Query {
         return Query("/$pattern/", field)
     }
 
+    /**
+     * Creates a range query.
+     *
+     * @param left The left value to search
+     * @param right The right value to search
+     * @param leftInclusion The left value is included in the range
+     * @param rightInclusion The right value is included in the range
+     * @param field Indexed field to search
+     */
     fun range(left: String, right: String, leftInclusion: Boolean = true, rightInclusion : Boolean = true, field:String? = null): Query {
         val leftBracket = if (leftInclusion) "[" else "{"
         val rightBracket = if (rightInclusion) "]" else "}"
@@ -57,22 +91,52 @@ class QueryStringQueryOptionDsl {
         return Query("$leftBracket$leftExp TO $rightExp$rightBracket", field)
     }
 
-    fun fuzzy(value: String, count: Int, field: String? = null): Query {
-        return Query("\"$value\"~$count", field)
+    /**
+     * Creates a fuzzy query.
+     *
+     * @param value The value to search
+     * @param maxEdits Maximum number of single-character edits required to match the specified search term.
+     * @param field indexed field to search
+     */
+    fun fuzzy(value: String, maxEdits: Int, field: String? = null): Query {
+        return Query("\"$value\"~$maxEdits", field)
     }
 
+    /**
+     * Creates a delimiters for subqueries.
+     *
+     * @param query The Query for subqueries.
+     */
     fun sub(query: Query): Query {
         return Query("(${query.value})", query.field)
     }
 
+    /**
+     * Creates an operator that indicates `NOT` boolean operator.
+     * Specified search value must be absent for a document to be included in the results.
+     *
+     * @param query The Query to apply.
+     */
     fun not(query: Query): Query {
         return Query("NOT (${query.value})", query.field)
     }
 
+    /**
+     * Creates an operator that indicates `AND` boolean operator.
+     * All search values must be present for a document to be included in the results.
+     *
+     * @param query The Query to apply.
+     */
     infix fun Query.and(query: Query): Query {
         return Query("$this AND $query", null)
     }
 
+    /**
+     * Creates an operator that indicates OR boolean operator.
+     * At least one of the search value must be present for a document to be included in the results.
+     *
+     * @param query The Query to apply.
+     */
     infix fun Query.or(query: Query): Query {
         return Query("$this OR $query", null)
     }
