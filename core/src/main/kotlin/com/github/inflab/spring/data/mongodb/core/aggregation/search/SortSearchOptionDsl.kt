54943e96740a97d04e807af26985484e44964e65
Jake Son <sonjeabin@gmail.com>
2023-10-07 17:52:07 +0900

    feat: add sort option to search dsl

---
+++ core/src/main/kotlin/com/github/inflab/spring/data/mongodb/core/aggregation/search/SortSearchOptionDsl.kt
@@ -0,0 +1,149 @@
+package com.github.inflab.spring.data.mongodb.core.aggregation.search
+
+import com.github.inflab.spring.data.mongodb.core.annotation.AggregationMarker
+import com.github.inflab.spring.data.mongodb.core.extension.toDotPath
+import org.bson.Document
+import java.time.temporal.Temporal
+import kotlin.reflect.KProperty
+
+/**
+ * A Kotlin DSL to configure sort search option using idiomatic Kotlin code.
+ *
+ * @author Jake Son
+ * @since 1.0
+ * @see <a href="https://www.mongodb.com/docs/atlas/atlas-search/sort">Sort Atlas Search Results</a>
+ */
+@AggregationMarker
+class SortSearchOptionDsl {
+    private val options = mutableListOf<SortOption>()
+
+    internal sealed interface SortOption {
+        data class Field(val field: String, val order: Order) : SortOption
+        data class Score(val order: Order) : SortOption
+    }
+
+    /**
+     * Represents the sort order.
+     *
+     * @property value The sort order.
+     */
+    enum class Order(val value: Int) {
+        ASC(1),
+        DESC(-1),
+    }
+
+    /**
+     * Sort by the score.
+     */
+    object Score
+
+    /**
+     * Sort by the score.
+     */
+    val score: Score
+        get() = Score
+
+    /**
+     * Sort in ascending order.
+     * When you sort in ascending order, Atlas Search returns documents with missing values before documents with values.
+     */
+    val asc: Order
+        get() = Order.ASC
+
+    /**
+     * Sort in descending order.
+     */
+    val desc: Order
+        get() = Order.DESC
+
+    /**
+     * Specifies the sort order for the results.
+     *
+     * @param order The sort order.
+     */
+    infix fun String.by(order: Order) {
+        options += SortOption.Field(field = this, order = order)
+    }
+
+    /**
+     * Specifies the sort order for the results.
+     *
+     * @param order The sort order.
+     */
+    infix fun Score.by(order: Order) {
+        options += SortOption.Score(order = order)
+    }
+
+    /**
+     * Specifies the sort order for the results.
+     *
+     * @param order The sort order.
+     */
+    @JvmName("byTemporal")
+    infix fun KProperty<Temporal?>.by(order: Order) = this.toDotPath() by order
+
+    /**
+     * Specifies the sort order for the results.
+     *
+     * @param order The sort order.
+     */
+    @JvmName("byTemporalIterable")
+    infix fun KProperty<Iterable<Temporal?>?>.by(order: Order) = this.toDotPath() by order
+
+    /**
+     * Specifies the sort order for the results.
+     *
+     * @param order The sort order.
+     */
+    @JvmName("byNumber")
+    infix fun KProperty<Number?>.by(order: Order) = this.toDotPath() by order
+
+    /**
+     * Specifies the sort order for the results.
+     *
+     * @param order The sort order.
+     */
+    @JvmName("byNumberIterable")
+    infix fun KProperty<Iterable<Number?>?>.by(order: Order) = this.toDotPath() by order
+
+    /**
+     * Specifies the sort order for the results.
+     *
+     * @param order The sort order.
+     */
+    @JvmName("byString")
+    infix fun KProperty<String?>.by(order: Order) = this.toDotPath() by order
+
+    /**
+     * Specifies the sort order for the results.
+     *
+     * @param order The sort order.
+     */
+    @JvmName("byStringIterable")
+    infix fun KProperty<Iterable<String?>?>.by(order: Order) = this.toDotPath() by order
+
+    internal fun build(): Document {
+        val document = Document()
+
+        options.forEach {
+            when (it) {
+                is SortOption.Field -> document[it.field] = it.order.value
+                is SortOption.Score -> document.addScore(it.order)
+            }
+        }
+
+        return document
+    }
+
+    private fun Document.addScore(order: Order) {
+        val allFiledNames = options.filterIsInstance<SortOption.Field>().map { it.field }
+        val notDuplicatedName = (0..allFiledNames.size)
+            .asSequence()
+            .map { "unused$it" }
+            .first { !allFiledNames.contains(it) }
+
+        this[notDuplicatedName] = Document("\$meta", "searchScore").apply {
+            if (order == Order.ASC) put("order", order.value)
+        }
+    }
+}
---
+++ core/src/test/kotlin/com/github/inflab/spring/data/mongodb/core/aggregation/search/SortSearchOptionDslTest.kt
@@ -0,0 +1,129 @@
+package com.github.inflab.spring.data.mongodb.core.aggregation.search
+
+import com.github.inflab.spring.data.mongodb.core.util.shouldBeJson
+import io.kotest.core.spec.style.FreeSpec
+import java.time.temporal.Temporal
+
+internal class SortSearchOptionDslTest : FreeSpec({
+    fun sort(block: SortSearchOptionDsl.() -> Unit) =
+        SortSearchOptionDsl().apply(block)
+
+    "should choose a name that does not conflict with all field names" {
+        // given
+        val option = sort {
+            score by asc
+            "unused0" by asc
+            "unused1" by desc
+            "unused2" by asc
+            "unused3" by desc
+            "unused4" by asc
+        }
+
+        // when
+        val result = option.build()
+
+        // then
+        result.shouldBeJson(
+            """
+            {
+              "unused5": {
+                "${"$"}meta": "searchScore",
+                "order": 1
+              },
+              "unused0": 1,
+              "unused1": -1,
+              "unused2": 1,
+              "unused3": -1,
+              "unused4": 1
+            }
+            """.trimIndent(),
+        )
+    }
+
+    "should build an ascending order" {
+        // given
+        data class Collection(
+            val string: String?,
+            val number: Number?,
+            val temporal: Temporal?,
+            val stringList: List<String?>?,
+            val numberList: List<Number?>?,
+            val temporalList: List<Temporal?>?,
+        )
+        val option = sort {
+            "field" by asc
+            score by asc
+            Collection::string by asc
+            Collection::number by asc
+            Collection::temporal by asc
+            Collection::stringList by asc
+            Collection::numberList by asc
+            Collection::temporalList by asc
+        }
+
+        // when
+        val result = option.build()
+
+        // then
+        result.shouldBeJson(
+            """
+                {
+                  "field": 1,
+                  "unused0": {
+                    "${"$"}meta": "searchScore",
+                    "order": 1
+                  },
+                  "string": 1,
+                  "number": 1,
+                  "temporal": 1,
+                  "stringList": 1,
+                  "numberList": 1,
+                  "temporalList": 1
+                }
+            """.trimIndent(),
+        )
+    }
+
+    "should build a descending order" {
+        // given
+        data class Collection(
+            val string: String?,
+            val number: Number?,
+            val temporal: Temporal?,
+            val stringList: List<String?>?,
+            val numberList: List<Number?>?,
+            val temporalList: List<Temporal?>?,
+        )
+        val option = sort {
+            "field" by desc
+            score by desc
+            Collection::string by desc
+            Collection::number by desc
+            Collection::temporal by desc
+            Collection::stringList by desc
+            Collection::numberList by desc
+            Collection::temporalList by desc
+        }
+
+        // when
+        val result = option.build()
+
+        // then
+        result.shouldBeJson(
+            """
+                {
+                  "field": -1,
+                  "unused0": {
+                    "${"$"}meta": "searchScore"
+                  },
+                  "string": -1,
+                  "number": -1,
+                  "temporal": -1,
+                  "stringList": -1,
+                  "numberList": -1,
+                  "temporalList": -1
+                }
+            """.trimIndent(),
+        )
+    }
+})
