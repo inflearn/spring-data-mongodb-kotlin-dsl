d046f0c17e744b38457c19a54e31a19c8759ae15
Jake Son <sonjeabin@gmail.com>
2023-10-08 16:39:33 +0900

    feat: implement $add expression dsl

---
+++ core/src/main/kotlin/com/github/inflab/spring/data/mongodb/core/aggregation/expression/arithmetic/AddExpressionDsl.kt
@@ -0,0 +1,107 @@
+package com.github.inflab.spring.data.mongodb.core.aggregation.expression.arithmetic
+
+import com.github.inflab.spring.data.mongodb.core.aggregation.expression.AggregationExpressionDsl
+import com.github.inflab.spring.data.mongodb.core.annotation.AggregationMarker
+import com.github.inflab.spring.data.mongodb.core.extension.toDotPath
+import org.springframework.data.mongodb.core.aggregation.AggregationExpression
+import java.time.temporal.Temporal
+import kotlin.reflect.KProperty
+
+/**
+ * A Kotlin DSL to configure `$add` [AggregationExpression] using idiomatic Kotlin code.
+ *
+ * @author Jake Son
+ * @since 1.0
+ * @see <a href="https://docs.mongodb.com/manual/reference/operator/aggregation/add">$add</a>
+ */
+@AggregationMarker
+class AddExpressionDsl {
+    /**
+     * Represents all operands inside `$add` as a list.
+     */
+    @JvmInline
+    value class Operands(val values: MutableList<Any>)
+
+    /**
+     * Creates a [Operands] with the given [value].
+     *
+     * @param value The number.
+     */
+    fun of(value: Number) = Operands(mutableListOf(value))
+
+    /**
+     * Creates a [Operands] with the given [date].
+     *
+     * @param date The date.
+     */
+    fun of(date: Temporal) = Operands(mutableListOf(date))
+
+    /**
+     * Creates a [Operands] with the given [field].
+     *
+     * @param field The name of the field.
+     */
+    fun of(field: String) = Operands(mutableListOf("$$field"))
+
+    /**
+     * Creates a [Operands] with the given [field].
+     *
+     * @param field The name of the field.
+     */
+    fun of(field: KProperty<*>) = of(field.toDotPath())
+
+    /**
+     * Creates a [Operands] with the given [AggregationExpression].
+     *
+     * @param configuration The configuration block for the [AggregationExpressionDsl].
+     */
+    fun of(configuration: AggregationExpressionDsl.() -> AggregationExpression) =
+        Operands(mutableListOf(AggregationExpressionDsl().configuration().toDocument()))
+
+    /**
+     * Adds [value] to the list of operands.
+     *
+     * @param value The number.
+     */
+    infix fun Operands.and(value: Number): Operands {
+        values.add(value)
+        return this
+    }
+
+    /**
+     * Adds [date] to the list of operands.
+     *
+     * @param date The date.
+     */
+    infix fun Operands.and(date: Temporal): Operands {
+        values.add(date)
+        return this
+    }
+
+    /**
+     * Adds [field] to the list of operands.
+     *
+     * @param field The name of the field.
+     */
+    infix fun Operands.and(field: String): Operands {
+        values.add("$$field")
+        return this
+    }
+
+    /**
+     * Adds [field] to the list of operands.
+     *
+     * @param field The name of the field.
+     */
+    infix fun Operands.and(field: KProperty<*>): Operands = and(field.toDotPath())
+
+    /**
+     * Adds [AggregationExpression] to the list of operands.
+     *
+     * @param configuration The configuration block for the [AggregationExpressionDsl].
+     */
+    infix fun Operands.and(configuration: AggregationExpressionDsl.() -> AggregationExpression): Operands {
+        values.add(AggregationExpressionDsl().configuration().toDocument())
+        return this
+    }
+}
