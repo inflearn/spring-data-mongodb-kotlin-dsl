730e997b34102d0dac8dd8dad15f066692a95edc
username1103 <mikim961103@gmail.com>
2023-10-28 14:35:08 +0900

    feat: add field stage dsl repository test

---
+++ example/spring-data-mongodb/src/main/kotlin/com/github/inflab/example/spring/data/mongodb/repository/AddFieldsRepository.kt
@@ -0,0 +1,71 @@
+package com.github.inflab.example.spring.data.mongodb.repository
+
+import com.github.inflab.spring.data.mongodb.core.aggregation.aggregation
+import org.springframework.data.mongodb.core.MongoTemplate
+import org.springframework.data.mongodb.core.aggregate
+import org.springframework.data.mongodb.core.aggregation.AggregationResults
+import org.springframework.data.mongodb.core.mapping.Field
+import org.springframework.stereotype.Repository
+
+@Repository
+class AddFieldsRepository(
+    private val mongoTemplate: MongoTemplate,
+) {
+
+    data class Specs(val doors: Long?, val wheels: Long?, @Field("fuel_type") val fuelType: String?)
+    data class Vehicle(val id: Long, val type: String, val specs: Specs?)
+
+    data class Animal(val id: Long, val dogs: Long, val cats: Long)
+
+    data class Fruit(@Field("_id") val id: String, val item: String, val type: String)
+
+    // TODO: https://www.mongodb.com/docs/v7.0/reference/operator/aggregation/addFields/#using-two--addfields-stages
+
+    /**
+     * @see <a href="https://www.mongodb.com/docs/v7.0/reference/operator/aggregation/addFields/#adding-fields-to-an-embedded-document">Adding Fields to an Embedded Document</a>
+     */
+    fun addFieldsToEmbeddedDocument(): AggregationResults<Vehicle> {
+        val aggregation = aggregation {
+            addFields {
+                "specs.fuel_type" set "unleaded"
+            }
+        }
+
+        return mongoTemplate.aggregate<Vehicle>(aggregation, VEHICLES)
+    }
+
+    /**
+     * @see <a href="https://www.mongodb.com/docs/v7.0/reference/operator/aggregation/addFields/#overwriting-an-existing-field">Overwriting an existing field</a>
+     */
+    fun overwriteAnExistingFieldWithValue(): AggregationResults<Animal> {
+        val aggregation = aggregation {
+            addFields {
+                Animal::cats set 20
+            }
+        }
+
+        return mongoTemplate.aggregate<Animal>(aggregation, ANIMALS)
+    }
+
+    /**
+     * @see <a href="https://www.mongodb.com/docs/v7.0/reference/operator/aggregation/addFields/#overwriting-an-existing-field">Overwriting an existing field</a>
+     */
+    fun overwriteAnExistingFieldWithField(): AggregationResults<Fruit> {
+        val aggregation = aggregation {
+            addFields {
+                Fruit::id set Fruit::item
+                Fruit::item set "fruit"
+            }
+        }
+
+        return mongoTemplate.aggregate<Fruit>(aggregation, FRUITS)
+    }
+
+    // TODO: https://www.mongodb.com/docs/v7.0/reference/operator/aggregation/addFields/#add-element-to-an-array
+
+    companion object {
+        const val VEHICLES = "vehicles"
+        const val ANIMALS = "animals"
+        const val FRUITS = "fruits"
+    }
+}
