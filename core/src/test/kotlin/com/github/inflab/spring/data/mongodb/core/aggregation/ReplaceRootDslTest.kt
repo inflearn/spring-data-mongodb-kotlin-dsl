e5ff07419e7fc6e2be24ae63ed8093b5b123bfcb
username1103 <mikim961103@gmail.com>
2023-11-05 21:46:43 +0900

    feat: test replace root dsl

--- core/src/test/kotlin/com/github/inflab/spring/data/mongodb/core/aggregation/ReplaceRootDslTest.kt
+++ core/src/test/kotlin/com/github/inflab/spring/data/mongodb/core/aggregation/ReplaceRootDslTest.kt
@@ -39,7 +39,254 @@ class ReplaceRootDslTest : FreeSpec({
                 }
                 """.trimIndent(),
             )
+        }
 
+        "should set field with path" {
+            // given
+            data class Test(val a: Int, val targetPath: Int)
+            val stage = replaceRoot {
+                newRoot {
+                    "a" set Test::targetPath
+                }
+            }
+
+            // when
+            val result = stage.get()
+
+            // then
+            result.shouldBeJson(
+                """
+                {
+                  "${'$'}replaceRoot": {
+                    "newRoot": {
+                      "a": "${'$'}targetPath"
+                    }
+                  }
+                }
+                """.trimIndent(),
+            )
+        }
+
+        "should set field with value" {
+            // given
+            val stage = replaceRoot {
+                newRoot {
+                    "a" set 1
+                }
+            }
+
+            // when
+            val result = stage.get()
+
+            // then
+            result.shouldBeJson(
+                """
+                {
+                  "${'$'}replaceRoot": {
+                    "newRoot": {
+                      "a": 1
+                    }
+                  }
+                }
+                """.trimIndent(),
+            )
+        }
+
+        "should set field with value of other field" {
+            // given
+            val stage = replaceRoot {
+                newRoot {
+                    "a" setByField "b"
+                }
+            }
+
+            // when
+            val result = stage.get()
+
+            // then
+            result.shouldBeJson(
+                """
+                {
+                  "${'$'}replaceRoot": {
+                    "newRoot": {
+                      "a": "${'$'}b"
+                    }
+                  }
+                }
+                """.trimIndent(),
+            )
+        }
+
+        "should set nested field" {
+            // given
+            data class Test(val a: Long, val b: Long, val c: Long, val d: Long)
+            val stage = replaceRoot {
+                newRoot {
+                    nested("a") {
+                        "b" set Test::d
+                        "c" set {
+                            add { of(1) and 2 }
+                        }
+                        "d" setByField "c"
+                        nested("e") {
+                            "f" set 1
+                        }
+                    }
+                }
+            }
+
+            // when
+            val result = stage.get()
+
+            // then
+            result.shouldBeJson(
+                """
+                {
+                  "${'$'}replaceRoot": {
+                    "newRoot": {
+                      "a.b": "${'$'}d",
+                      "a.c": {
+                        "${'$'}add": [
+                          1,
+                          2
+                        ]
+                      },
+                      "a.d": "${'$'}c",
+                      "a.e.f": 1
+                    }
+                  }
+                }
+                """.trimIndent(),
+            )
+        }
+
+        "should set field path with aggregation expression" {
+            // given
+            data class Test(val sourcePath: Int, val targetPath: Int)
+            val stage = replaceRoot {
+                newRoot {
+                    Test::targetPath set {
+                        add { of(1) and 2 and Test::sourcePath }
+                    }
+                }
+            }
+
+            // when
+            val result = stage.get()
+
+            // then
+            result.shouldBeJson(
+                """
+                {
+                  "${'$'}replaceRoot": {
+                    "newRoot": {
+                      "targetPath": {
+                        "${'$'}add": [
+                          1,
+                          2,
+                          "${'$'}sourcePath"
+                        ]
+                      }
+                    }
+                  }
+                }
+                """.trimIndent(),
+            )
+        }
+
+        "should set field path with path" {
+            // given
+            data class Test(val sourcePath: Int, val targetPath: Int)
+            val stage = replaceRoot {
+                newRoot {
+                    Test::targetPath set Test::sourcePath
+                }
+            }
+
+            // when
+            val result = stage.get()
+
+            // then
+            result.shouldBeJson(
+                """
+                {
+                  "${'$'}replaceRoot": {
+                    "newRoot": {
+                      "targetPath": "${'$'}sourcePath"
+                    }
+                  }
+                }
+                """.trimIndent(),
+            )
+        }
+
+        "should set field path with value" {
+            // given
+            data class Test(val sourcePath: Int, val targetPath: Int)
+            val stage = replaceRoot {
+                newRoot {
+                    Test::targetPath set 1
+                }
+            }
+
+            // when
+            val result = stage.get()
+
+            // then
+            result.shouldBeJson(
+                """
+                {
+                  "${'$'}replaceRoot": {
+                    "newRoot": {
+                      "targetPath": 1
+                    }
+                  }
+                }
+                """.trimIndent(),
+            )
+        }
+
+        "should set nested field path" {
+            // given
+            data class Test(val a: Long, val b: Long, val c: Long, val d: Long)
+            val stage = replaceRoot {
+                newRoot {
+                    nested(Test::a) {
+                        Test::b set Test::d
+                        Test::c set {
+                            add { of(1) and 2 }
+                        }
+                        Test::d setByField "c"
+                        nested("e") {
+                            "f" set 1
+                        }
+                    }
+                }
+            }
+
+            // when
+            val result = stage.get()
+
+            // then
+            result.shouldBeJson(
+                """
+                {
+                  "${'$'}replaceRoot": {
+                    "newRoot": {
+                      "a.b": "${'$'}d",
+                      "a.c": {
+                        "${'$'}add": [
+                          1,
+                          2
+                        ]
+                      },
+                      "a.d": "${'$'}c",
+                      "a.e.f": 1
+                    }
+                  }
+                }
+                """.trimIndent(),
+            )
         }
     }
 })
